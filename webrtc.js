c39b9a7cfa9ecca4f1d720e7d4ed275a844a5ea6
ThalusA <alexandre.monier@epitech.eu>
2019-12-23 22:29:43 +0100

    Add STUN servers

--- webrtc.js
+++ webrtc.js
@@ -5,7 +5,24 @@ let startTime = null;
 
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
-const servers = null;
+const servers = {
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.ekiga.net" },
+        { urls: "stun:stun.ideasip.com" },
+        { urls: "stun:stun.rixtelecom.se" },
+        { urls: "stun:stun.schlund.de" },
+        { urls: "stun:stun.stunprotocol.org:3478" },
+        { urls: "stun:stun.voiparound.com" },
+        { urls: "stun:stun.voipbuster.com" },
+        { urls: "stun:stun.voipstunt.com" },
+        { urls: "stun:stun.voxgratia.org" }
+    ]
+};
 
 let localStream;
 let remoteStream;
